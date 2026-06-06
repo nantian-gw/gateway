bdece3659caf323349d0cd8caca24ccb4a1abd9f
mahmut <aymenjacquet.x@gmail.com>
2026-06-06 14:54:40 +0800

    remove some doc
    Signed-off-by: mahmut <aymenjacquet.x@gmail.com>

--- tests/scripts/fixtures/rust-unwraps/allowed_runtime.rs
+++
@@ -1,5 +0,0 @@
-#![forbid(unsafe_code)]
-
-pub fn parse_number(input: &str) -> Result<u64, std::num::ParseIntError> {
-    input.parse()
-}
--- tests/scripts/fixtures/rust-unwraps/disallowed_runtime.rs
+++
@@ -1,5 +0,0 @@
-#![forbid(unsafe_code)]
-
-pub fn parse_number(input: &str) -> u64 {
-    input.parse::<u64>().unwrap()
-}
